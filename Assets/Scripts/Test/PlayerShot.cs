d4a4c131b6813d2c6088a3237b322b8b7b9b7e0a
cross <ne280107@senshu-u.jp>
2018-05-23 19:51:27 +0900

    Unityには気をつけよう！
    2018版以外はころせ

--- Assets/Scripts/Test/PlayerShot.cs
+++ Assets/Scripts/Test/PlayerShot.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class PlayerShot : MonoBehaviour {
 
-	// bullet prefab
+    // bullet prefab
     public GameObject bullet;
- 
+
     // 弾丸発射点
     public Transform muzzle;
- 
+
     // 弾丸の速度
     public float speed = 1000;
- 
-    // Use this for initialization
-    void Start () {
- 
+
+    void Start()
+    {
+
     }
- 
-    // Update is called once per frame
-    void Update () {
+
+    void Update()
+    {
         // z キーが押された時
-        if(Input.GetKeyDown (KeyCode.Z)){
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
             // 弾丸の複製
-            GameObject bullets = GameObject.Instantiate(bullet)as GameObject;
- 
+            GameObject bullets = GameObject.Instantiate(bullet) as GameObject;
+
             Vector3 force;
             force = this.gameObject.transform.up * speed;
             // Rigidbodyに力を加えて発射
-            bullets.GetComponent<Rigidbody>().AddForce (force);
+            bullets.GetComponent<Rigidbody>().AddForce(force);
             // 弾丸の位置を調整
             bullets.transform.position = muzzle.position;
         }
