2ed09702b42c6f827b1faaf30a94d94fd4844ffe
cross <ne280107@senshu-u.jp>
2018-07-18 19:00:13 +0900

    2Pの生成
    取りあえず1Pと2Pが生成されて動ける

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : PlayerManager {
-
+public class Player : MonoBehaviour
+{
     public int HP;
     public float SPD;
-    public float shot_SPD;
-    public float shot_interval;
-    public float NS_ATK;
-    public float SS_ATK;
 
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        switch(this.tag)
+        {
+            case "Player1":
+                if (Input.GetKey(KeyCode.W)) { this.transform.position += Vector3.forward * SPD; }
+                if (Input.GetKey(KeyCode.S)) { this.transform.position += Vector3.back * SPD; }
+                if (Input.GetKey(KeyCode.D)) { this.transform.position += Vector3.right * SPD; }
+                if (Input.GetKey(KeyCode.A)) { this.transform.position += Vector3.left * SPD; }
+                break;
+
+            case "Player2":
+                if (Input.GetKey(KeyCode.UpArrow)) { this.transform.position += Vector3.forward * SPD; }
+                if (Input.GetKey(KeyCode.DownArrow)) { this.transform.position += Vector3.back * SPD; }
+                if (Input.GetKey(KeyCode.RightArrow)) { this.transform.position += Vector3.right * SPD; }
+                if (Input.GetKey(KeyCode.LeftArrow)) { this.transform.position += Vector3.left * SPD; }
+                break;
+        }
+    }
 }
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -12,57 +12,80 @@ public class PlayerManager : MonoBehaviour
         Anoma,
     }
 
+    public enum Player2
+    {
+        Veronica,
+        Anoma,
+    }
+
     public Player1 player1;
 
     // プレイヤー１のステータス
     public int player1_HP;
     public float player1_SPD;
-    public float player1_shot_SPD;
-    public float player1_shot_interval;
-    public float player1_NS_ATK;
-    public float player1_SS_ATK;
+
+    public Player2 player2;
+
+    // プレイヤー２のステータス;
+    public int player2_HP;
+    public float player2_SPD;
 
     private void OnValidate()
     {
         player1_HP = Mathf.Max (1, player1_HP);
+        player2_HP = Mathf.Max(1, player2_HP);
 
         // キャラごとのデフォルトステータス
         switch(player1)
         {
             case Player1.Veronica:
                 player1_HP = 100;
-                player1_SPD = 3;
-                player1_NS_ATK = 10;
-                player1_SS_ATK = 30;
+                player1_SPD = 0.3f;
                 break;
 
             case Player1.Anoma:
                 player1_HP = 200;
-                player1_SPD = 2;
-                player1_NS_ATK = 5;
-                player1_SS_ATK = 20;
+                player1_SPD = 0.2f;
                 break;
         }
-    }
 
-    private void awake()
-    {
-        gameObject.GetComponent<Player>().HP = player1_HP;
-        gameObject.GetComponent<Player>().SPD = player1_SPD;
-        gameObject.GetComponent<Player>().shot_SPD = player1_shot_SPD;
-        gameObject.GetComponent<Player>().shot_interval = player1_shot_SPD;
-        gameObject.GetComponent<Player>().NS_ATK = player1_NS_ATK;
-        gameObject.GetComponent<Player>().SS_ATK = player1_SS_ATK;
+        switch(player2)
+        {
+            case Player2.Veronica:
+                player2_HP = 100;
+                player2_SPD = 0.3f;
+                break;
+
+            case Player2.Anoma:
+                player2_HP = 200;
+                player2_SPD = 0.2f;
+                break;
+        }
     }
 
     private void Start()
     {
         string P1name = player1.ToString();
+        string P2name = player2.ToString();
         // キャラの生成
-        GameObject P1 = Instantiate((GameObject)Resources.Load("Models/" + P1name)) as GameObject;
-        P1.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-        P1.transform.position = new Vector3(0, 0, -2.25f);
-        P1.transform.rotation = Quaternion.Euler(90, 0, 0);
+        GameObject char1 = Instantiate((GameObject)Resources.Load("Models/" + P1name)) as GameObject;
+        char1.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        char1.transform.position = new Vector3(-2.25f, 0, -2.25f);
+        char1.transform.rotation = Quaternion.Euler(90, 0, 0);
+        GameObject char2 = Instantiate((GameObject)Resources.Load("Models/" + P2name)) as GameObject;
+        char2.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        char2.transform.position = new Vector3(2.25f, 0, -2.25f);
+        char2.transform.rotation = Quaternion.Euler(90, 0, 0);
 
+        // ステータスの割り振り
+        char1.AddComponent<Player>();
+        char1.GetComponent<Player>().HP = player1_HP;
+        char1.GetComponent<Player>().SPD = player1_SPD;
+        char1.tag = "Player1";
+        char2.AddComponent<Player>();
+        char2.GetComponent<Player>().HP = player2_HP;
+        char2.GetComponent<Player>().SPD = player2_SPD;
+        char2.tag = "Player2";
     }
+
 }
