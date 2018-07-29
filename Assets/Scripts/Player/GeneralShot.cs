cb07c0c44b765b06d16914c154859184746f2ac2
cross <ne280107@senshu-u.jp>
2018-07-29 11:07:36 +0900

    オーソドックスな弾の動きを作った
    それとちょっとPlayerについて試行錯誤

---
+++ Assets/Scripts/Player/GeneralShot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralShot : MonoBehaviour {
+
+    public GameObject normalBullet; // 通常弾のプレハブ
+
+    public int shotInterval = 3; // ショットの間隔
+
+    int timeCount; // 弾の間隔を管理
+	
+	private void Update ()
+    {
+        // z キーを押している間
+        if (Input.GetKey(KeyCode.Z))
+        {
+            timeCount++;
+            if(timeCount > shotInterval)
+            {
+                timeCount = 0;
+
+                GameObject normalBullets = Instantiate(normalBullet) as GameObject; // 弾の生成
+                normalBullets.transform.position = this.transform.position; // 弾の発射点を更新
+            }
+
+        }
+	}
+}
